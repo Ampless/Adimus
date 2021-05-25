b1ff5858a489f4c0bf3b4e401ca8b59ebb856e67
chris <chrissx@chrissx.de>
2021-05-25 02:07:19 +0200

    yaml looks way better than json for most things, lets use it

--- bin/adimus.dart
+++ bin/adimus.dart
@@ -1,36 +1,46 @@
-import 'dart:convert';
 import 'dart:io';
+import 'package:fhir_yaml/fhir_yaml.dart';
 import 'package:html/dom.dart' as dom;
-import 'package:html/parser.dart';
 import 'package:html_search/html_search.dart' as html;
 import 'package:schttp/schttp.dart';
+import 'package:yaml/yaml.dart';
 
 final http = ScHttpClient();
 
-Iterable<dom.Element> searchHtml(List<dom.Element> root, String className) =>
-    html.search(root, (e) => e.className.contains(className));
+Iterable<dom.Element> searchClass(List<dom.Element> root, String className) =>
+    html.search(root, (e) => e.className == className);
+
+String searchClassLastInnerClean(List<dom.Element> root, String className) =>
+    searchClass(root, className)
+        .last
+        .innerHtml
+        .replaceAll(RegExp('</?[a-z ="]+>'), ' ')
+        .replaceAll(RegExp(' +'), ' ')
+        .trim();
 
 Iterable<String> htmlGetAnchors(List<dom.Element> root) => html
     .search(root, (e) => e.attributes.containsKey('href'))
     .map((e) => '$endpoint${e.attributes['href']}');
 
-dom.Element? htmlGetRightTr(List<dom.Element> root) =>
-    html.searchFirst(root, (e) => e.outerHtml.startsWith('<table>'));
-
 Future<Iterable<String>> getBaseUrls(String url) async =>
-    searchHtml(HtmlParser(await http.get(url)).parse().children, 'einruecken')
+    searchClass(html.parse(await http.get(url)), 'einruecken')
         .map((e) => '$endpoint${e.attributes['href']}');
 
-Future<Map<String, String>> getRealHtml(Iterable<String> urls) async {
+Future<Map<String, String>> words(Iterable<String> urls) async {
   final h = <String, String>{};
   for (final u in urls) {
-    print(u);
-    final elements = HtmlParser(await http.get(u)).parse().children;
-    final subs = searchHtml(elements, 'p-substitute');
+    final elements = html.parse(await http.get(u));
+    final subs = searchClass(elements, 'p-substitute');
     if (subs.isNotEmpty) {
-      h.addAll(await getRealHtml(htmlGetAnchors(subs.first.children)));
+      h.addAll(await words(htmlGetAnchors(subs.first.children)));
     } else {
-      h[u] = htmlGetRightTr(elements)!.children.last.innerHtml;
+      final table = html
+          .searchFirst(elements, (e) => e.outerHtml.startsWith('<table>'))!
+          .children;
+      final latin = searchClassLastInnerClean(table, 'eh2');
+      final german = searchClassLastInnerClean(table, 'eh');
+      h[latin] = german;
+      print('$latin = $german ($u)');
     }
   }
   return h;
@@ -38,14 +48,14 @@ Future<Map<String, String>> getRealHtml(Iterable<String> urls) async {
 
 const endpoint = 'https://www.frag-caesar.de';
 const listUrl = '$endpoint/lateinwoerterbuch/beginnend-mit-a.html';
-final cache = File('adimus.json');
+final cache = File('adimus.yaml');
 
 void main() async {
   final dict = await cache.exists()
-      ? Map<String, String>.from(jsonDecode(await cache.readAsString()))
-      : await getRealHtml(await getBaseUrls(listUrl));
+      ? Map<String, String>.from(loadYaml(await cache.readAsString()))
+      : await words(await getBaseUrls(listUrl));
   dict.removeWhere((k, v) => v.isEmpty);
   dict.forEach((k, v) => dict[k] =
       v.trim().replaceAll('<br>', ' ').replaceAll(RegExp('<.*?>'), ''));
-  await cache.writeAsString(jsonEncode(dict));
+  await cache.writeAsString(json2yaml(dict));
 }
