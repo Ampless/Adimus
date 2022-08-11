674a134bfc0c85e63aed1caef72cb1ca41de7ee4
pixel <pixel@chrissx.de>
2022-08-12 00:20:28 +0200

    update dependencies and a lot of improvements

--- bin/adimus.dart
+++ bin/adimus.dart
@@ -1,49 +1,45 @@
 import 'dart:io';
 import 'package:fhir_yaml/fhir_yaml.dart';
 import 'package:html/dom.dart' as dom;
-import 'package:html_search/html_search.dart' as html;
-import 'package:schttp/schttp.dart';
+import 'package:html_search/html_search.dart';
+import 'package:http/http.dart';
 import 'package:yaml/yaml.dart';
 
-final http = ScHttpClient();
-
-Iterable<dom.Element> searchClass(List<dom.Element> root, String className) =>
-    html.search(root, (e) => e.className == className);
-
-String searchClassLastInnerClean(List<dom.Element> root, String className) =>
-    searchClass(root, className)
-        .last
-        .innerHtml
-        .replaceAll(RegExp('</?[a-z ="]+>'), ' ')
-        .replaceAll(RegExp(' +'), ' ')
-        .trim();
-
-Iterable<String> htmlGetAnchors(List<dom.Element> root) => html
-    .search(root, (e) => e.attributes.containsKey('href'))
+Iterable<String> htmlGetAnchors(List<dom.Element> root) => root
+    .search((e) => e.attributes.containsKey('href'))
     .map((e) => '$endpoint${e.attributes['href']}');
 
 Future<Iterable<String>> getBaseUrls(String url) async =>
-    searchClass(html.parse(await http.get(url)), 'einruecken')
+    htmlParse(await get(Uri.parse(url)).then((r) => r.body))
+        .search((e) => e.className == 'einruecken')
         .map((e) => '$endpoint${e.attributes['href']}');
 
-Future<Map<String, String>> words(Iterable<String> urls) async {
-  final h = <String, String>{};
+Stream<MapEntry<String, String>> words(Iterable<String> urls) async* {
   for (final u in urls) {
-    final elements = html.parse(await http.get(u));
-    final subs = searchClass(elements, 'p-substitute');
+    final res = await get(Uri.parse(u));
+    print('${res.statusCode} $u');
+    if (res.statusCode != 200) {
+      continue;
+    }
+    final elements = htmlParse(res.body);
+    final subs = elements.search((e) => e.className == 'p-substitute');
     if (subs.isNotEmpty) {
-      h.addAll(await words(htmlGetAnchors(subs.first.children)));
+      yield* words(htmlGetAnchors(subs.first.children));
     } else {
-      final table = html
-          .searchFirst(elements, (e) => e.outerHtml.startsWith('<table>'))!
+      final table = elements
+          .searchFirst((e) => e.outerHtml.startsWith('<table'))!
           .children;
-      final latin = searchClassLastInnerClean(table, 'eh2');
-      final german = searchClassLastInnerClean(table, 'eh');
-      h[latin] = german;
-      print('$latin = $german ($u)');
+      String searchClassLastInnerClean(String cn) => table
+          .search((e) => e.className == cn)
+          .last
+          .innerHtml
+          .replaceAll(RegExp('</?[a-z ="]+>'), ' ')
+          .replaceAll(RegExp(' +'), ' ')
+          .trim();
+      yield MapEntry(
+          searchClassLastInnerClean('eh2'), searchClassLastInnerClean('eh'));
     }
   }
-  return h;
 }
 
 const endpoint = 'https://www.frag-caesar.de';
@@ -53,7 +49,7 @@ final cache = File('adimus.yaml');
 void main() async {
   final dict = await cache.exists()
       ? Map<String, String>.from(loadYaml(await cache.readAsString()))
-      : await words(await getBaseUrls(listUrl));
+      : await words(await getBaseUrls(listUrl)).toList().then(Map.fromEntries);
   dict.removeWhere((k, v) => v.isEmpty);
   dict.forEach((k, v) => dict[k] =
       v.trim().replaceAll('<br>', ' ').replaceAll(RegExp('<.*?>'), ''));
